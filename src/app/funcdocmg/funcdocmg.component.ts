0f14668b0eac6a2fda4a8549c7a7bf39546dd117
Anayss <lycanays@gmail.com>
2018-05-26 19:29:43 +0800

    0

---
+++ src/app/funcdocmg/funcdocmg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-funcdocmg',
+  templateUrl: './funcdocmg.component.html',
+  styleUrls: ['./funcdocmg.component.css']
+})
+export class FuncdocmgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/projectmg/projectmg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projectmg',
+  templateUrl: './projectmg.component.html',
+  styleUrls: ['./projectmg.component.css']
+})
+export class ProjectmgComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
