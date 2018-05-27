a2ba9aede8641c14d064c287c9a73b202b7b7f79
Anayss <lycanays@gmail.com>
2018-05-27 15:33:53 +0800

    0

--- src/app/funcdocmg/funcdocmg.component.ts
+++ src/app/funcdocmg/funcdocmg.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./funcdocmg.component.css']
 })
 export class FuncdocmgComponent implements OnInit {
-
+  private funcpoints:Array<funcpoint>;
   constructor() { }
+  isUpdateTR=1;
+
 
   ngOnInit() {
+    this.funcpoints=[
+      new funcpoint("功能点1","一","外部逻辑"),
+      new funcpoint("功能点2","一","内部链接"),
+      new funcpoint("功能点3","二","外部逻辑"),
+    ]
   }
+  
+}
+export class funcpoint{
+  constructor(
+    public name:String,
+    public module:String,
+    public node:String
+  ){}
+  
 
 }
