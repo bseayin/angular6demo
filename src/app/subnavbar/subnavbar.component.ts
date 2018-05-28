bef4892c9025577cfab512f653aaa5f4bfea6920
djwzzw <1424818476@qq.com>
2018-05-28 16:42:49 +0800

    djw 点击按钮改变样式

--- src/app/subnavbar/subnavbar.component.ts
+++ src/app/subnavbar/subnavbar.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubnavbarComponent implements OnInit {
 
+item1="nav-link "
+item2="nav-link "
+item3="nav-link "
+item4="nav-link "
+item5="nav-link "
   constructor() { }
 
   ngOnInit() {
+  }
+  clickItem2(item:any){
+    this.item1="nav-link";
+    this.item2="nav-link";
+    this.item3="nav-link";
+    this.item4="nav-link";
+    this.item5="nav-link";
+if("A"==item){
+  this.item1="nav-link active";
+}
+if("B"==item){
+  this.item2="nav-link active";
+}
+if("C"==item){
+  this.item3="nav-link active";
+}
+if("D"==item){
+  this.item4="nav-link active";
+}
+if("E"==item){
+  this.item5="nav-link active";
+}
   }
 
 }
