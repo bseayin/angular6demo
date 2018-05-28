c49102c329c81e7d1a631a88d39ebb52efd0e145
ybacon <294392502@qq.com>
2018-05-28 22:12:52 +0800

    完善

--- src/app/teamfiles/teamfiles.component.ts
+++ src/app/teamfiles/teamfiles.component.ts
@@ -10,11 +10,6 @@ export class TeamfilesComponent implements OnInit {
   private content:String
   constructor() { }
   isUpdateTR=1; 
-  itemClass1="list-group-item list-group-item-action";
-  itemClass2="list-group-item list-group-item-action";
-  itemClass3="list-group-item list-group-item-action";
-  itemClass4="list-group-item list-group-item-action";
-  itemClass5="list-group-item list-group-item-action";
   ngOnInit() {
     this.splitplans=[
       new splitplan(1,"冲刺1","天工开物","1111-11-11","1111-11-11","内容1","未开始"),
@@ -23,25 +18,6 @@ export class TeamfilesComponent implements OnInit {
       new splitplan(4,"冲刺4","天工开物","1111-11-11","1111-11-11","内容4","未开始"),
       new splitplan(5,"冲刺5","天工开物","1111-11-11","1111-11-11","内容5","未开始")
     ]
-  }
-  clickItem(item:any){
-    this.itemClass1="list-group-item list-group-item-action";
-    this.itemClass2="list-group-item list-group-item-action";
-    this.itemClass3="list-group-item list-group-item-action";
-    this.itemClass4="list-group-item list-group-item-action";
-    this.itemClass5="list-group-item list-group-item-action";
-if("A"==item){
-  this.itemClass1="list-group-item list-group-item-action active";
-}
-if("B"==item){
-  this.itemClass2="list-group-item list-group-item-action active";
-}
-if("C"==item){
-  this.itemClass3="list-group-item list-group-item-action active";
-}
-if("D"==item){
-  this.itemClass4="list-group-item list-group-item-action active";
-}
   }
   showcontent(content1){
    this.content=content1.content;
