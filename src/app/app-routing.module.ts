22956ff85e93b1ec28a6af02ce5418b216e476b8
bseayin <yinyouhai@aliyun.com>
2018-06-20 21:01:19 +0800

    部署

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -61,7 +61,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {'useHash': true})
   ],
   exports: [ RouterModule ],
   declarations: []
